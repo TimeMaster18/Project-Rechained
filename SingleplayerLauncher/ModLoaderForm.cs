ac222bdad62d833d6ea6286ee9f7ab6029acfb70
MopsieX <ryanmorici96@gmail.com>
2019-08-23 00:23:15 -0400

    Added Mod Loader form
    Created a new form and put god mode under it.

---
+++ SingleplayerLauncher/ModLoaderForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SingleplayerLauncher
+{
+    public partial class ModLoaderForm : Form
+    {
+        public ModLoaderForm()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void ModLoaderForm_Load(object sender, EventArgs e)
+        {
+            chkGodMode.Checked = Settings.Instance.ContainsKey("GodMode")? (bool)Settings.Instance["GodMode"] : false;
+        }
+        private void chkGodMode_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkGodMode.Checked)
+            {
+                Settings.Instance["GodMode"] = true;
+                Settings.Save();
+            }
+            else
+            {
+                Settings.Instance["GodMode"] = false;
+                Settings.Save();
+            }
+        }
+    }
+}
