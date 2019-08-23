15b78c842c9e26a195b7ee3cb74afa71ce4b2657
MopsieX <ryanmorici96@gmail.com>
2019-08-23 00:40:13 -0400

    No trap cap mod

--- SingleplayerLauncher/ModLoaderForm.cs
+++ SingleplayerLauncher/ModLoaderForm.cs
@@ -20,6 +20,7 @@ namespace SingleplayerLauncher
         private void ModLoaderForm_Load(object sender, EventArgs e)
         {
             chkGodMode.Checked = Settings.Instance.ContainsKey("GodMode")? (bool)Settings.Instance["GodMode"] : false;
+            chkNoTrapCap.Checked = Settings.Instance.ContainsKey("NoTrapCap") ? (bool)Settings.Instance["NoTrapCap"] : false;
         }
         private void chkGodMode_CheckedChanged(object sender, EventArgs e)
         {
@@ -31,6 +32,20 @@ namespace SingleplayerLauncher
             else
             {
                 Settings.Instance["GodMode"] = false;
+                Settings.Save();
+            }
+        }
+
+        private void chkNoTrapCap_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkNoTrapCap.Checked)
+            {
+                Settings.Instance["NoTrapCap"] = true;
+                Settings.Save();
+            }
+            else
+            {
+                Settings.Instance["NoTrapCap"] = false;
                 Settings.Save();
             }
         }
